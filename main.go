42f86a65ba6fa358c957cc1b07279d263ae0880c
Yamauchi Masato <masato.yamauchi@livesense.co.jp>
2025-03-27 16:39:11 +0900

    add opt

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"ip-info/handlers/root"
 	"log"
 
@@ -8,11 +10,15 @@ import (
 )
 
 func main() {
+	host := flag.String("h", "", "host")
+	port := flag.String("p", "8080", "port")
+	flag.Parse()
+
 	r := gin.Default()
 
 	r.GET("/", root.Index)
 
-	if err := r.Run(":8080"); err != nil {
+	if err := r.Run(fmt.Sprintf("%s:%s", *host, *port)); err != nil {
 		log.Fatal(err)
 	}
 }
