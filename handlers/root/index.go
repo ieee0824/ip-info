8fc6f574b7c67c29b9cb6d8128e2c3b19a9ae682
虚無 <ieee0824x@gmail.com>
2025-04-03 06:22:25 +0900

    add ua

--- handlers/root/index.go
+++ handlers/root/index.go
@@ -4,6 +4,7 @@ import "github.com/gin-gonic/gin"
 
 func Index(c *gin.Context) {
 	c.JSON(200, gin.H{
-		"ip": c.ClientIP(),
+		"ip":         c.ClientIP(),
+		"user-agent": c.Request.UserAgent(),
 	})
 }
