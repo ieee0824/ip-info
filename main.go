cdd6724ef5c8b11b73dd578f1317fcbb5ae3175e
虚無 <ieee0824x@gmail.com>
2025-04-03 07:58:20 +0900

    with ssl

--- main.go
+++ main.go
@@ -12,13 +12,23 @@ import (
 func main() {
 	host := flag.String("h", "", "host")
 	port := flag.String("p", "8080", "port")
+	withSSL := flag.Bool("ssl", false, "use ssl")
+	cert := flag.String("cert", "", "ssl cert")
+	key := flag.String("key", "", "ssl key")
 	flag.Parse()
 
 	r := gin.Default()
 
 	r.GET("/", root.Index)
 
-	if err := r.Run(fmt.Sprintf("%s:%s", *host, *port)); err != nil {
-		log.Fatal(err)
+	if *withSSL {
+		if err := r.RunTLS(fmt.Sprintf("%s:%s", *host, *port), *cert, *key); err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		if err := r.Run(fmt.Sprintf("%s:%s", *host, *port)); err != nil {
+			log.Fatal(err)
+		}
 	}
+
 }
