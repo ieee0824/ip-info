8ba2e8921624e5addd1c346def4cd09fecf86bfa
虚無 <ieee0824x@gmail.com>
2025-04-03 08:11:42 +0900

    use pid file

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"ip-info/handlers/root"
 	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
@@ -15,6 +16,22 @@ func main() {
 	withSSL := flag.Bool("ssl", false, "use ssl")
 	cert := flag.String("cert", "", "ssl cert")
 	key := flag.String("key", "", "ssl key")
+
+	withPid := flag.Bool("pid", false, "write pid to file")
+	pidFile := flag.String("pid-file", "/run/ip-info.pid", "pid file")
+
+	if *withPid {
+		pid := os.Getpid()
+		if err := os.WriteFile(*pidFile, []byte(fmt.Sprintf("%d", pid)), 0644); err != nil {
+			log.Fatalf("failed to write pid file: %v", err)
+		}
+		defer func() {
+			if err := os.Remove(*pidFile); err != nil {
+				log.Printf("failed to remove pid file: %v", err)
+			}
+		}()
+	}
+
 	flag.Parse()
 
 	r := gin.Default()
